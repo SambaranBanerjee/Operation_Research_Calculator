b4f9c906eb4e5eeb32b57bc8e9baeed5036433c1
SambaranBanerjee <sanbaranb25@gmail.com>
2025-11-10 19:55:13 +0530

    Removed history and added the logo for the app

--- Calcify/app/(tabs)/_layout.tsx
+++ Calcify/app/(tabs)/_layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Tabs } from 'expo-router'
 import Entypo from 'react-native-vector-icons/Entypo';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const _layout = () => {
   return (
@@ -16,20 +15,6 @@ const _layout = () => {
           headerTitleStyle: { color: 'white' },
           tabBarIcon: ({ color, size }) => (
             <Entypo name="home" color={color} size={size} />
-          ),
-          tabBarStyle: { backgroundColor: '#121212' },
-        }}
-      />
-      <Tabs.Screen
-        name='history'
-        options={{
-          title: 'History',
-          tabBarLabel: 'History',
-          headerShown: true,
-          headerStyle: { backgroundColor: '#1f1f1f'},
-          headerTitleStyle: { color: 'white' },
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="history" color={color} size={size} />
           ),
           tabBarStyle: { backgroundColor: '#121212' },
         }}
